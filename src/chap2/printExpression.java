6f836897eb43bd4a58fee056b3f40fc65f6b241e
한혜숙 <qew9546@naver.com>
2021-07-12 20:00:44 +0900

    test2

---
+++ src/chap2/printExpression.java
@@ -0,0 +1,10 @@
+package chap2;
+
+public class printExpression {
+    public static void main(String[] args) {
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/3);
+    }
+}
