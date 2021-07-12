c784d998ea2d4cc44f6c5d31392e36380f4eb0b0
한혜숙 <qew9546@naver.com>
2021-07-12 20:16:01 +0900

    .

---
+++ src/chap2/CreateVar.java
@@ -0,0 +1,13 @@
+package chap2;
+
+public class CreateVar {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
